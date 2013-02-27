24ae5b70dca6fc05ff6693a60cab467378a45911
Abhishek Patil <abhishek@thezeroth.net>
2013-02-27 17:05:02 +0100

    Path were absolute which is not good when you deploy so make it kind of relative
    Signed-off-by: Abhishek Patil <abhishek@thezeroth.net>

--- vite.py
+++ vite.py
@@ -30,18 +30,21 @@ class Page:
 def get_path(dir_name, source = os.getcwd()):
     _current_path = source
     _path = ""
+#    print "dir_name " + dir_name + "  " + "_current_path " + _current_path
     if(os.path.exists(os.path.join(_current_path, dir_name))):
-        _path = os.path.join(_current_path, dir_name)
+        _path = _current_path + "/" + dir_name#os.path.join(_current_path, dir_name)
     return _path
 
 def file_dir_exists(name):
-    return os.path.exists(get_path(name))
+    return os.path.exists(name)
 
 def create_dir(source, new_dir_name):
     if not file_dir_exists(os.path.join(source, new_dir_name)):
         os.mkdir(os.path.join(source, new_dir_name))
     return  get_path(new_dir_name, source)
    
+def get_file_path(filename, source):
+    return source + "/" + filename
 #Global objects
 templates = ""
 dirs = ()
@@ -73,34 +76,43 @@ def process_dir(dir_name):
     #check / create site dir
     if not file_dir_exists("site"):
         os.mkdir(os.path.join(os.getcwd(), "site"))
-    _source_dir = get_path(dir_name)
-    _dest_dir = create_dir(get_path("site"), dir_name)
+    _source_dir = dir_name#get_path(dir_name)
+    _dest_dir = create_dir("site", dir_name)#get_path("site"), dir_name)
     _files = os.listdir(_source_dir)
     for f in _files:
-        page, content =  process_file(get_path(f, _source_dir), _dest_dir)
+        page, content =  process_file(get_path(f, _source_dir), _dest_dir)#get_path(f, _source_dir), _dest_dir)
         #save page
+        print "F : " + f
+        print "_source_dir " + _source_dir
+        print "_dest_dir " + _dest_dir
+
         _dest_file_name = page.title.strip().lower().replace(" ", "_").strip()
         _dest_file_path = _dest_dir+"/"+_dest_file_name+".html"
         _dest_file = open(_dest_file_path, 'w')
         _dest_file.write(content)
         _dest_file.close()
         global site
-        page.url = _dest_file_path
+        page.url = _source_dir+ "/"+_dest_file_name+".html"
         site.pages.append(page)
     create_index()
 
 def create_index():
-    jinja_env = Environment(loader=FileSystemLoader(os.getcwd()))
+    jinja_env = Environment(loader=FileSystemLoader(get_path(templates)))
+ #   template = jinja_env.get_template(page.layout.strip())
+
+#    jinja_env = Environment(loader=FileSystemLoader(os.getcwd()))
     template = jinja_env.get_template("index.html")
     global site
     final_data = template.render(site=site)
-    s = get_path("site")
+    s = "site"
     _dest_file = s + "/" + "index.html" 
     f = open(_dest_file, 'w')
     f.write(final_data)
     f.close()
 
 def process_file(file_name, dest_dir):
+    print "file Name " + file_name
+    #print "dest Name " + dest_dir
     mf = open(file_name, 'r')
     if not mf:
         print file_name + " cann't be open";
